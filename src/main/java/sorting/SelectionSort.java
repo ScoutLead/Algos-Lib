a3ad17eb5591136fd7cf47d7c862f7085690b54a
yurii.kachmar <yurii.kachmar@eleks.com>
2017-09-04 00:17:09 +0300

    Sorting: Selection sort and unit test

---
+++ src/main/java/sorting/SelectionSort.java
@@ -0,0 +1,16 @@
+package sorting;
+
+public class SelectionSort {
+    public static <T extends Comparable<T>> T []  sort (T [] array) {
+        for(int j = 1; j < array.length; j++) {
+            T key = array[j];
+            int i = j - 1;
+            while(i > -1 && array[i].compareTo(key) > 0) {
+                array[i + 1] = array[i];
+                i = i - 1;
+            }
+            array[i + 1] = key;
+        }
+        return array;
+    }
+}
