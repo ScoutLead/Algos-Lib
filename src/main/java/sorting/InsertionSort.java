6dd1a39cc24a3a923fa4c029b05335023ac5bff1
yurii.kachmar <yurii.kachmar@eleks.com>
2017-09-04 00:18:32 +0300

    Sorting: Insertion sort and unit test

--- src/main/java/sorting/InsertionSort.java
+++ src/main/java/sorting/InsertionSort.java
@@ -1,6 +1,6 @@
 package sorting;
 
-public class SelectionSort {
+public class InsertionSort {
     public static <T extends Comparable<T>> T []  sort (T [] array) {
         for(int j = 1; j < array.length; j++) {
             T key = array[j];
--- src/test/java/sorting/InsertionSortTest.java
+++ src/test/java/sorting/InsertionSortTest.java
@@ -4,14 +4,14 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-public class SelectionSortTest {
+public class InsertionSortTest {
     @Test
     public void Sort_ArrayIsSorted_IntegerArray() {
         //Arrange
         Integer[] array = {4, 3, 2, 1};
         Integer[] expectedArray = {1, 2, 3, 4};
         //Act
-        Integer[] actualArray =  SelectionSort.sort(array);
+        Integer[] actualArray =  InsertionSort.sort(array);
         //Assert
         assertArrayEquals(expectedArray, actualArray);
     }
